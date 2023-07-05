dcebedbf4260ede919674829e674d8454656344f
Hüseyin Kaplan <113541206+huseyinkaplann@users.noreply.github.com>
2023-07-05 03:07:35 +0300

    Update contact_import.py

--- contact_import.py
+++ contact_import.py
@@ -4,7 +4,7 @@ from csv2vcard import csv2vcard
 data = pd.read_excel("vcard.xlsx")
 data.drop_duplicates(subset="Phone Number", inplace=True, keep="last")
 
-with open("contacts.csv", "a", encoding="UTF-8") as file:
+with open("contacts.csv", "w", encoding="UTF-8") as file:
     file.write("last_name,first_name,org,title,phone,email,website,street,city,p_code,country" + "\n")
     for i in range(len(data)):
         file.write(f",{str(data.iloc[i]['Name']).title()},,,{data.iloc[i]['Phone Number']},,,,,," + "\n")
